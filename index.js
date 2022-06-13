06af8930756ba5bedbda4be8ffa63ad90c0b79af
Gwali <gwalisam37@gmail.com>
2022-06-12 18:46:42 -0700

    modified index.js

--- index.js
+++ index.js
@@ -24,9 +24,7 @@ const boot = function(){
 
     if (sessionStorage){
         for(let i = 0 ; i <  sessionStorage.length-takeAway ; i++){
-            console.log(i);
             content+= `<li style = color:red;>${sessionStorage.getItem(`name${i}`)}</li>`;
-            console.log(sessionStorage.getItem(`name${i}`));
         }
         populate(content);    
     }
@@ -50,17 +48,12 @@ btn.addEventListener("click",function (e){
     e.preventDefault()
     sessionStorage.setItem(`name${counter}`,namez.value);
     counter+=1;
-    //store each name recieved in sessionStorage to be used to populate in time of  refresh
     sessionStorage.setItem("counter",counter)
-
-    //using text node to insert into document instead of array and innerHtml
     const ele = document.createElement("li")
     ele.style.color = "red"
     const textnode = document.createTextNode(namez.value)
     ele .appendChild(textnode)
     document.querySelector(".people").appendChild(ele)
-    
-    //reset input field
     namez.value =""
     document.querySelector(".person").setAttribute("autofocus","autofocus");
 })
